7a0041e658888688257c1c3081bb9ec291197c1b
BernardDev <bernard.devworks@gmail.com>
2021-04-06 16:26:33 +0200

    Setup signup route & redirect to users page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,5 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
-    'Hello World'
   end
 end
---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,18 @@
+class UserController < ApplicationController
+
+get '/users/signup' do
+    erb :'/users/signup'
+end
+
+post '/users/signup' do
+    @user = User.create(username: params[:username], password: params[:password])
+    # binding.pry
+    redirect "/users/#{@user.id}"
+end
+
+get '/users/:id' do
+    # "your personal page"
+    @user = User.find(params[:id])
+end
+
+end
